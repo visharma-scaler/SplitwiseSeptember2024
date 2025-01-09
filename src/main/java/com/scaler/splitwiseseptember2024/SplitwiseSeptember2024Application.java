b547478c0110591974923a5dbabc71e1a4a79f52
visharma-scaler <vishal.sharma@scaler.com>
2025-01-09 23:41:23 +0530

    [LLD][Machine Coding][Design Splitwise]: Add Problem Statement

---
+++ src/main/java/com/scaler/splitwiseseptember2024/SplitwiseSeptember2024Application.java
@@ -0,0 +1,13 @@
+package com.scaler.splitwiseseptember2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseSeptember2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseSeptember2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/splitwiseseptember2024/SplitwiseSeptember2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.splitwiseseptember2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseSeptember2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
